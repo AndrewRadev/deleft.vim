6af3680c31b1ae0c70cbdb0fc55e3ac492e8e2cd
Andrew Radev <andrey.radev@gmail.com>
2021-02-15 12:25:05 +0200

    Vendor Rust support for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,17 @@ require_relative './support/vim'
 Vimrunner::RSpec.configure do |config|
   config.reuse_server = true
 
-  plugin_path = File.expand_path('.')
+  plugin_path = Pathname.new(File.expand_path('.'))
 
   config.start_vim do
     vim = Vimrunner.start_gvim
     vim.add_plugin(plugin_path, 'plugin/deleft.vim')
+
+    # Up-to-date filetype support:
+    vim.prepend_runtimepath(plugin_path.join('spec/support/rust.vim'))
+
+    # bootstrap filetypes
+    vim.command 'autocmd BufNewFile,BufRead *.rs set filetype=rust'
 
     if vim.echo('exists(":packadd")').to_i > 0
       vim.command('packadd matchit')
